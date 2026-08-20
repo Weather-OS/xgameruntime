7bcac23d751ed5e0a6a3acfb512f9c42cf939c14
Weather-OS <tpapata1386@gmail.com>
2026-08-20 23:20:56 +0330

    xasync.h: Fix `internal` declaration within XAsyncBlock.

--- include/xasync.h
+++ include/xasync.h
@@ -30,13 +30,15 @@ typedef struct XAsyncBlock XAsyncBlock;
 typedef void    __stdcall XAsyncCompletionRoutine( XAsyncBlock *asyncBlock );
 typedef HRESULT __stdcall XAsyncWork( XAsyncBlock *asyncBlock );
 
+#ifndef __WIDL__
 struct XAsyncBlock
 {
     XTaskQueueHandle queue;
     void *context;
     XAsyncCompletionRoutine *callback;
-    void *internal[4];
+    unsigned internal[sizeof(void*) * 4];
 };
+#endif
 
 HRESULT __stdcall XAsyncGetStatus( XAsyncBlock *asyncBlock, BOOLEAN wait );
 HRESULT __stdcall XAsyncGetResultSize( XAsyncBlock *asyncBlock, SIZE_T *bufferSize );
