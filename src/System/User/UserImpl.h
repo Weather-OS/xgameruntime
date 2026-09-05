f458166c3187783c4709c3330ede0eeb48e44485
Weather-OS <tpapata1386@gmail.com>
2026-09-05 15:19:49 +0330

    xgameruntime: Stub various components from winegdk.

---
+++ src/System/User/UserImpl.h
@@ -0,0 +1,67 @@
+/*
+ * Xbox Game runtime Library
+ *  GDK Component: System API -> XNetworking
+ * 
+ * Written by Weather
+ * Copyright 2026 Olivia Ryan
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+#ifndef USERIMPL_H
+#define USERIMPL_H
+
+#include "../../../private.h"
+#include "provider.h"
+
+#include <atomic>
+
+using namespace ABI::XGameRuntime;
+
+struct XUser
+{
+    UINT32 m_signature;
+    IUser* m_user;
+};
+
+struct XUserAddContext
+{
+    XUserAddOptions options;
+    XUserHandle user;
+    HANDLE userAddEvent;
+};
+
+class ABI::XGameRuntime::UserImpl : 
+    public IUser
+{
+public:
+    UserImpl( HSTRING token );
+    virtual ~UserImpl() = default;
+
+    HRESULT WINAPI QueryInterface( REFIID iid, void **out ) noexcept override;
+    ULONG WINAPI AddRef() noexcept override;
+    ULONG WINAPI Release() noexcept override;
+
+    HRESULT WINAPI get_MsaToken( HSTRING *out ) override;
+
+private:
+    HSTRING m_token;
+    std::atomic_long ref{ 1 };
+};
+
+// Signatures can be anything because these are not exposed to the client.
+static uint32_t const X_USER_SIGNATURE = 0xa7f3c92d;
+
+#endif
